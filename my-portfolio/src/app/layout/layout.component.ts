19988a180bb14538434afd63a43dcffbb00d6d56
Kyle Jaworski <KyleJaworski1@gmail.com>
2025-01-13 15:11:25 -0500

    Update layout.component.ts

--- my-portfolio/src/app/layout/layout.component.ts
+++ my-portfolio/src/app/layout/layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { HeroSectionComponent } from '../hero-section/hero-section.component';
+import { TimelineComponent } from '../timeline/timeline.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [NavBarComponent, HeroSectionComponent],
+  imports: [NavBarComponent, HeroSectionComponent, TimelineComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
