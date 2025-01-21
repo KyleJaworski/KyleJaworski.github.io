d97c1c1251be20dcffee404baf2b3fc0b587d113
Kyle Jaworski <KyleJaworski1@gmail.com>
2025-01-21 16:10:47 -0500

    Pic change

--- my-portfolio/src/app/hero-section/hero-section.component.ts
+++ my-portfolio/src/app/hero-section/hero-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css',
 })
-export class HeroSectionComponent {
+export class HeroSectionComponent implements OnInit {
+  heroImage: string = '';
+
   imageToDisplay(): string {
     const randomNum: Number = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
@@ -19,6 +21,25 @@ export class HeroSectionComponent {
         return 'assets/images/hero_image_3.jpg';
       default:
         return 'assets/images/hero_image.jpg';
+    }
+  }
+
+  ngOnInit(): void {
+    const randomNum: Number = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+    switch (randomNum) {
+      case 1:
+        this.heroImage = 'assets/images/hero_image.jpg';
+        break;
+      case 2:
+        this.heroImage = 'assets/images/hero_image_2.jpg';
+        break;
+      case 3:
+        this.heroImage = 'assets/images/hero_image_3.jpg';
+        break;
+      default:
+        this.heroImage = 'assets/images/hero_image.jpg';
+        break;
     }
   }
 }
